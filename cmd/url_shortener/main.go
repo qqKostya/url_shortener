491988d4bf37e42d4e67e4c8a8059fb8904d9e80
qqKazakov <leap74rus@gmail.com>
2024-10-28 13:16:22 +0300

    add: plan and todo

---
+++ cmd/url_shortener/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//TODO: init config: cleanenv
+	//TODO: init logger: slog
+	//TODO: init storege: sqlite
+	//TODO: init router: chi, "chi render"
+	//TODO: start server
+}
