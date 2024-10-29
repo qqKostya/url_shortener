bde7177942244f9a803ae9b5e3e517c33078a206
qqKazakov <leap74rus@gmail.com>
2024-10-29 12:46:33 +0300

    add: storage sqlite

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"os"
 	"url_shortener/internal/config"
+	"url_shortener/internal/lib/logger/sl"
+	"url_shortener/internal/storage/sqlite"
 )
 
 const (
@@ -22,6 +24,12 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	//TODO: init storege: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 
 	//TODO: init router: chi, "chi render"
 
