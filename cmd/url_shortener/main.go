20cbd89294bbb1db0343e8ac1c8e32a902bb173c
qqKazakov <leap74rus@gmail.com>
2024-10-29 11:42:58 +0300

    add: config parse

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"url_shortener/internal/config"
+)
+
 func main() {
-	//TODO: init config: cleanenv
+	//TODO: init config: cleanenv +
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	//TODO: init logger: slog
 	//TODO: init storege: sqlite
 	//TODO: init router: chi, "chi render"
