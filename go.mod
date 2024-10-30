0d886c8d7998bc4a4ddcfe70c481a76646efabe2
qqKazakov <leap74rus@gmail.com>
2024-10-30 12:50:35 +0300

    add: handler save

--- go.mod
+++ go.mod
@@ -5,8 +5,21 @@ go 1.22.1
 require github.com/fatih/color v1.18.0
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
+
+require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-playground/validator/v10 v10.22.1
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
---
+++ internal/http-server/handlers/url/save.go
@@ -0,0 +1,54 @@
+package url
+
+import (
+	"log/slog"
+	"net/http"
+	resp "url_shortener/internal/lib/api/response"
+	"url_shortener/internal/lib/logger/sl"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias, omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias, omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		op := "handlers.url.save.New"
+
+		log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+			render.JSON(w, r, resp.Error("failed to decode request"))
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("failed to validate request body", sl.Err(err))
+			render.JSON(w, r, resp.ValidationError(err.(validator.ValidationErrors)))
+			return
+		}
+	}
+}
